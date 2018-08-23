0d2b2645a5d7d667bf9c7f759ba7c245c51c312c
Sonya Luvita <gungunpriatna02@gmail.com>
2018-08-23 09:36:03 +0700

    first commit :tada:

---
+++ app/src/main/java/me/gungunpriatna/stacknotif/NotificationItem.java
@@ -0,0 +1,36 @@
+package me.gungunpriatna.stacknotif;
+
+public class NotificationItem {
+    private int id;
+    private String sender;
+    private String message;
+    public NotificationItem(int id, String sender, String message) {
+        this.id = id;
+        this.sender = sender;
+        this.message = message;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
